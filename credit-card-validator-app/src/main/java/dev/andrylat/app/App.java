ed7195b72d49d60a1ba464d7fece4d39717b0b29
raqimbek <raqimbek@proton.me>
2025-02-16 01:00:42 +0500

    implemented non-digit input exclusion

--- credit-card-validator-app/src/main/java/dev/andrylat/app/App.java
+++ credit-card-validator-app/src/main/java/dev/andrylat/app/App.java
@@ -1,10 +1,39 @@
 package dev.andrylat.app;
 
-/**
- * Hello world!
- */
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class App {
+    private static StringBuilder errors = new StringBuilder();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Hello. Enter card number for validation:");
+        var input = Arrays.stream(scanner.nextLine().split(""))
+                         .filter(App::isDigit)
+                         .map(Integer::valueOf)
+                         .toList();
+
+        if (errors.length() > 0) {
+            System.out.println("Card number is invalid.");
+            System.out.println("Errors:");
+            System.out.println(errors.toString());
+        } else {
+            System.out.println("Card is valid.");
+        }
+    }
+
+    private static boolean isDigit(String s) {
+        if (s.equals(" ")) return false;
+
+        try {
+            Integer.parseInt(s); 
+        } catch (NumberFormatException e) {
+            errors.append("-> Number should contain only digits\n");
+            return false;
+        }
+
+        return true;
     }
 }
