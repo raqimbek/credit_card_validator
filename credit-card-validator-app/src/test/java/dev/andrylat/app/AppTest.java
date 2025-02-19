7b13553111dafe66946a6d5339665f62c2e4edd0
raqimbek <raqimbek@proton.me>
2025-02-19 20:45:45 +0500

    added vim swap file to the .gitignore file and removed sample test

--- credit-card-validator-app/src/test/java/dev/andrylat/app/AppTest.java
+++ credit-card-validator-app/src/test/java/dev/andrylat/app/AppTest.java
@@ -4,16 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void shouldReturnTrue() {
+        ;
     }
 }
