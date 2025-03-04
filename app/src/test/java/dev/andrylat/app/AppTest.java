52aecc55ecfa3c6978583acb0050f3f479994aee
raqimbek <raqimbek@proton.me>
2025-03-04 20:49:31 +0500

    wrote the unit tests

--- app/src/test/java/dev/andrylat/app/AppTest.java
+++ app/src/test/java/dev/andrylat/app/AppTest.java
@@ -7,27 +7,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
+	
+	CreditCardHandler creditCardHandler = new CreditCardHandler();
 
     @Test
     public void shouldReturnStringContainingTwoErrors() {
         var expected = """
+Card number is invalid.
+Errors:
 -> Length should be 16 symbols
 -> Payment System can't be determined
-""";
+""";  
 
-        var app = new App().process();
-        assertEquals(app.getErrors().trim(), expected.trim());
+        assertEquals(creditCardHandler.check("1234").trim(), expected.trim());
     }
 
     @Test
     public void shouldReturnStringContainingThreeErrors() {
         var expected = """
+Card number is invalid.
+Errors:
 -> Length should be 16 symbols
 -> Number should contain only digits
 -> Payment System can't be determined
 """;
 
-        var app = new App().process();
-        assertEquals(app.getErrors().trim(), expected.trim());
+        assertEquals(creditCardHandler.check("12o1").trim(), expected.trim());
+    }
+    
+    @Test
+    public void shouldReturnStringContainingOneError() {
+        var expected = """
+Card number is invalid.
+Errors:
+-> Payment System can't be determined
+""";
+
+        assertEquals(creditCardHandler.check("5425 2334 3010 9923").trim(), expected.trim());
+    }
+    
+    @Test
+    public void shouldReturnStringContainingCreditCardBrandName() {
+        var expected = "Card is valid. Payment System is MASTERCARD";
+
+        assertEquals(creditCardHandler.check("5425 2334 3010 9903").trim(), expected.trim());
     }
 }
