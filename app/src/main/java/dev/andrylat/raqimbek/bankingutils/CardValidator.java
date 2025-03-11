aa7a0d164d518ad9e1a6ffeeea7a348f87cea548
raqimbek <raqimbek@proton.me>
2025-03-11 10:50:14 +0500

    renamed the method, fixed the bug with card number prefix check and implemented card number length check

--- app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidator.java
+++ app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidator.java
@@ -7,74 +7,95 @@ import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 public class CardValidator {
-	private static final int VALID_CREDIT_CARD_NUMBER_LENGTH = 16;
+    private static final int VALID_CREDIT_CARD_NUMBER_LENGTH = 16;
     private List<String> errors = new ArrayList<>();
     private StringBuilder message = new StringBuilder();
     private StringBuilder brand = new StringBuilder();
     private PaymentSystemDeterminer brandDeterminer = new PaymentSystemDeterminer();
-    
-    public CardValidationInfo checkCardNumber(String input) {
-    	validateCardNumber(input);
-    	
-    	var isValid = false;
-    	
-    	if (errors.size() == 0) {
-    		isValid = true;
-    		message.append("Card number is invalid.\n")
-                   .append("Errors:\n")
-    		       .append(errors.toString());
-    	} else {
-    		message.append("Card is valid. Payment System is ")
-    		       .append(brand);
-    	}
-    	
-    	return new CardValidationInfo(isValid, errors);
-    }
-    
-    private CardValidationInfo checkCardNumberPrefix(List<Integer> cardNumber) {
-    	var paymentSystemArray = PaymentSystem.values();
-    	var errors = new ArrayList<String>();
-    	
-    	for (PaymentSystem paymentSystem : paymentSystemArray) {
-    		var prefix = paymentSystem.getPrefixesAsLists();
-    		
-    		for (var i = 0; i < prefix.size(); i++) {
-    			for (var j = 0; j < prefix.get(i).size(); j++) {
-					if (cardNumber.get(j) == prefix.get(i).get(j)) {
-						return new CardValidationInfo(true, errors);
-					}
-    			}
-    		}
-    	}
 
-    	errors.add("Payment System prefix is not valid");
-    	return new CardValidationInfo(false, errors);
+    public CardValidationInfo checkCardNumber(String input) {
+        validateCardNumber(input);
+
+        var isValid = false;
+
+        if (errors.size() == 0) {
+            isValid = true;
+            message.append("Card number is invalid.\n").append("Errors:\n").append(errors.toString());
+        } else {
+            message.append("Card is valid. Payment System is ").append(brand);
+        }
+
+        return new CardValidationInfo(isValid, errors);
+    }
+
+    private CardValidationInfo conformsWithPaymentSystem(List<Integer> cardNumber) {
+        var paymentSystemArray = PaymentSystem.values();
+        var errors = new ArrayList<String>();
+
+        for (PaymentSystem paymentSystem : paymentSystemArray) {
+            var prefixMatchCounter = 0;
+            var prefixes = paymentSystem.getPrefixesAsLists();
+            var maxLength = paymentSystem.getCardMaxLength();
+            var minLength = paymentSystem.getCardMinLength();
+
+            if (cardNumber.size() < minLength) {
+                errors.add(new StringBuilder("Length should be at least ")
+                        .append(minLength)
+                        .append(" symbols")
+                        .toString());
+            }
+            
+            if (cardNumber.size() > maxLength) {
+                errors.add(new StringBuilder("Length should be at most ")
+                        .append(maxLength)
+                        .append(" symbols")
+                        .toString());
+            }
+
+            for (var i = 0; i < prefixes.size(); i++) {
+                var prefixPartMatchCounter = 0;
+
+                for (var j = 0; j < prefixes.get(i).size(); j++) {
+                    if (cardNumber.get(j) == prefixes.get(i).get(j)) {
+                        prefixPartMatchCounter++;
+                    }
+                }
+
+                if (prefixPartMatchCounter == prefixes.get(i).size()) {
+                    prefixMatchCounter++;
+                }
+            }
+
+            if (prefixMatchCounter != 1) {
+                errors.add("Payment System prefix is not valid");
+            }
+        }
+
+        if (errors.size() > 0) {
+            return new CardValidationInfo(false, errors);
+        }
+        
+        return new CardValidationInfo(true, errors);
+        
     }
 
     private void validateCardNumber(String input) {
         if (input.length() < VALID_CREDIT_CARD_NUMBER_LENGTH) {
-            errors.add(new StringBuilder("-> Length should be ")
-            		.append(VALID_CREDIT_CARD_NUMBER_LENGTH)
-            		.append(" symbols\n")
-            		.toString());
+            errors.add(new StringBuilder("-> Length should be ").append(VALID_CREDIT_CARD_NUMBER_LENGTH)
+                    .append(" symbols\n").toString());
         }
-        
+
         var cardNumber = getCardNumber(input);
         brand.append(brandDeterminer.determineCreditCardBrandByNumber(cardNumber));
 
         if (cardNumber.size() < VALID_CREDIT_CARD_NUMBER_LENGTH || brand.length() == 0) {
             errors.add("-> Payment System can't be determined\n");
         } else {
-            var everyOtherNumberList = new ArrayList<>(IntStream.range(0, cardNumber.size())
-                    .filter(n -> n % 2 == 0)
-                    .mapToObj(cardNumber::get)
-                    .toList());
+            var everyOtherNumberList = new ArrayList<>(
+                    IntStream.range(0, cardNumber.size()).filter(n -> n % 2 == 0).mapToObj(cardNumber::get).toList());
 
-            var numbersWithTwoDigits = everyOtherNumberList.stream()
-                    .map(n -> n*2)
-                    .filter(n -> n >= 10 && n < 100)
-                    .map(n -> n/2)
-                    .toList();
+            var numbersWithTwoDigits = everyOtherNumberList.stream().map(n -> n * 2).filter(n -> n >= 10 && n < 100)
+                    .map(n -> n / 2).toList();
 
             for (var i = 0; i < cardNumber.size(); i++) {
                 if (everyOtherNumberList.contains(cardNumber.get(i))) {
@@ -89,21 +110,11 @@ public class CardValidator {
                 }
             }
 
-            var sumOfNumbersWithTwoDigits = numbersWithTwoDigits.stream()
-                    .map(n -> n*2)
-                    .map(String::valueOf)
-                    .map(s -> Arrays.stream(s.split(""))
-                        .map(Integer::valueOf)
-                        .mapToInt(Integer::intValue)
-                        .sum()
-                    )
-                    .mapToInt(Integer::intValue)
-                    .sum();
+            var sumOfNumbersWithTwoDigits = numbersWithTwoDigits.stream().map(n -> n * 2).map(String::valueOf)
+                    .map(s -> Arrays.stream(s.split("")).map(Integer::valueOf).mapToInt(Integer::intValue).sum())
+                    .mapToInt(Integer::intValue).sum();
 
-            var sumOfEveryOtherNumber = everyOtherNumberList.stream()
-                .map(n->n*2)
-                .mapToInt(Integer::intValue)
-                .sum();
+            var sumOfEveryOtherNumber = everyOtherNumberList.stream().map(n -> n * 2).mapToInt(Integer::intValue).sum();
 
             var cardNumberSum = cardNumber.stream().mapToInt(Integer::intValue).sum();
 
@@ -116,16 +127,13 @@ public class CardValidator {
     }
 
     private List<Integer> getCardNumber(String s) {
-        return new ArrayList<>(Arrays.stream(s.split(""))
-                         .filter(c -> !c.equals(" "))
-                         .filter(this::isDigit)
-                         .map(Integer::valueOf)
-                         .toList());
+        return new ArrayList<>(Arrays.stream(s.split("")).filter(c -> !c.equals(" ")).filter(this::isDigit)
+                .map(Integer::valueOf).toList());
     }
-    
+
     private boolean isDigit(String s) {
         try {
-            Integer.parseInt(s); 
+            Integer.parseInt(s);
         } catch (NumberFormatException e) {
             var msg = "-> Number should contain only digits\n";
 
