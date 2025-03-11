43782805c00b33492bb81ce31b301f95bd9af698
raqimbek <raqimbek@proton.me>
2025-03-11 10:03:43 +0500

    refactored PaymentSystem enum to consider card length

--- app/src/main/java/dev/andrylat/raqimbek/bankingutils/PaymentSystem.java
+++ app/src/main/java/dev/andrylat/raqimbek/bankingutils/PaymentSystem.java
@@ -3,24 +3,42 @@ package dev.andrylat.raqimbek.bankingutils;
 import java.util.stream.IntStream;
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public enum PaymentSystem {
-    VISA(4),
-    MASTERCARD("51-55"),
-    DISCOVER(6011, 65),
-    DINERS_CLUB(36,38),
-    JCB(35),
-    AMERICAN_EXPRESS(34,37);
+    VISA("16", List.of(4)),
+    MASTERCARD("16", "51-55"),
+    DISCOVER("16", List.of(6011, 65)),
+    DINERS_CLUB("16", List.of(36,38)),
+    JCB("16", List.of(35)),
+    AMERICAN_EXPRESS("16", List.of(34,37));
 	
+	private int cardMinLength;
+	private int cardMaxLength;
 	private List<Integer> prefixList = new ArrayList<>();
 
-	private PaymentSystem(int... prefixes) {
-		for (var prefix : prefixes) {
-			prefixList.add(prefix);
+	private PaymentSystem(String cardLengthRange, List<Integer> prefixes) {
+        this(cardLengthRange);
+		prefixList.addAll(prefixes);
+	}
+	
+	private PaymentSystem(String cardLengthRange) {
+		if (cardLengthRange.indexOf("-") == -1) {
+			cardMaxLength = Integer.valueOf(cardLengthRange).intValue();
+		} else if (StringUtils.countMatches(cardLengthRange, "-") == 1) {
+			cardMinLength = Integer.valueOf(
+					cardLengthRange.substring(0, cardLengthRange.indexOf("-"))
+					).intValue();
+			cardMaxLength = Integer.valueOf(
+					cardLengthRange.substring(cardLengthRange.indexOf("-")+1)
+					).intValue();
 		}
+
+		// idk yet whether it should throw an exception if an argument contains more than one hyphen
 	}
 
-	private PaymentSystem(String prefixRange) {
+	private PaymentSystem(String cardLengthRange, String prefixRange) {
+		this(cardLengthRange);
 		if (prefixRange.indexOf("-") != -1) {
 			var rangeEnds = prefixRange.split("-");
 			IntStream.range(
@@ -32,6 +50,14 @@ public enum PaymentSystem {
 		}
 	}
 	
+	public int getCardMaxLength() {
+		return cardMaxLength;
+	}
+	
+	public int getCardMinLength() {
+		return cardMinLength;
+	}
+
 	public List<Integer> getPrefixes() {
 		return prefixList;
 	}
