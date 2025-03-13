b520703b8c7adfe6093f8011fb79c3afbc6937d1
raqimbek <raqimbek@proton.me>
2025-03-13 09:54:57 +0500

    implemented CardValidator.checkControlSum()

--- app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidator.java
+++ app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidator.java
@@ -80,6 +80,39 @@ public class CardValidator {
     }
 
     private CardValidator checkControlSum(List<Integer> cardNumber) {
+        var everyOtherNumberList = new ArrayList<>(
+                IntStream.range(0, cardNumber.size()).filter(n -> n % 2 == 0).mapToObj(cardNumber::get).toList());
+
+        var numbersWithTwoDigits = everyOtherNumberList.stream().map(n -> n * 2).filter(n -> n >= 10 && n < 100)
+                .map(n -> n / 2).toList();
+
+        for (var i = 0; i < cardNumber.size(); i++) {
+            if (everyOtherNumberList.contains(cardNumber.get(i))) {
+                cardNumber.remove(cardNumber.get(i));
+            }
+        }
+
+        for (var i = 0; i < everyOtherNumberList.size(); i++) {
+            if (numbersWithTwoDigits.contains(everyOtherNumberList.get(i))) {
+                everyOtherNumberList.remove(everyOtherNumberList.get(i));
+                i--;
+            }
+        }
+
+        var sumOfNumbersWithTwoDigits = numbersWithTwoDigits.stream().map(n -> n * 2).map(String::valueOf)
+                .map(s -> Arrays.stream(s.split("")).map(Integer::valueOf).mapToInt(Integer::intValue).sum())
+                .mapToInt(Integer::intValue).sum();
+
+        var sumOfEveryOtherNumber = everyOtherNumberList.stream().map(n -> n * 2).mapToInt(Integer::intValue).sum();
+
+        var cardNumberSum = cardNumber.stream().mapToInt(Integer::intValue).sum();
+
+        var sum = cardNumberSum + sumOfNumbersWithTwoDigits + sumOfEveryOtherNumber;
+
+        if (sum % 10 != 0) {
+            errors.add("-> Card Number is not valid");
+        }
+
         return this;
     }
 
@@ -95,38 +128,7 @@ public class CardValidator {
         if (cardNumber.size() < VALID_CREDIT_CARD_NUMBER_LENGTH || brand.length() == 0) {
             errors.add("-> Payment System can't be determined\n");
         } else {
-            var everyOtherNumberList = new ArrayList<>(
-                    IntStream.range(0, cardNumber.size()).filter(n -> n % 2 == 0).mapToObj(cardNumber::get).toList());
-
-            var numbersWithTwoDigits = everyOtherNumberList.stream().map(n -> n * 2).filter(n -> n >= 10 && n < 100)
-                    .map(n -> n / 2).toList();
-
-            for (var i = 0; i < cardNumber.size(); i++) {
-                if (everyOtherNumberList.contains(cardNumber.get(i))) {
-                    cardNumber.remove(cardNumber.get(i));
-                }
-            }
-
-            for (var i = 0; i < everyOtherNumberList.size(); i++) {
-                if (numbersWithTwoDigits.contains(everyOtherNumberList.get(i))) {
-                    everyOtherNumberList.remove(everyOtherNumberList.get(i));
-                    i--;
-                }
-            }
-
-            var sumOfNumbersWithTwoDigits = numbersWithTwoDigits.stream().map(n -> n * 2).map(String::valueOf)
-                    .map(s -> Arrays.stream(s.split("")).map(Integer::valueOf).mapToInt(Integer::intValue).sum())
-                    .mapToInt(Integer::intValue).sum();
-
-            var sumOfEveryOtherNumber = everyOtherNumberList.stream().map(n -> n * 2).mapToInt(Integer::intValue).sum();
-
-            var cardNumberSum = cardNumber.stream().mapToInt(Integer::intValue).sum();
-
-            var sum = cardNumberSum + sumOfNumbersWithTwoDigits + sumOfEveryOtherNumber;
-
-            if (sum % 10 != 0) {
-                errors.add("-> Payment System can't be determined\n");
-            }
+            
         }
     }
 
