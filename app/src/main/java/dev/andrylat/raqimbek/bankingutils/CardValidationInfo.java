da85d8961aa864299ed59afce2ce3c4eead7c7b0
raqimbek <raqimbek@proton.me>
2025-03-16 19:09:04 +0500

    removed unnecessary paymentSystem field and its getter

--- app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidationInfo.java
+++ app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidationInfo.java
@@ -1,25 +1,18 @@
 package dev.andrylat.raqimbek.bankingutils;
 
 import java.util.List;
-import java.util.Optional;
 
 public class CardValidationInfo {
     boolean isValid;
-    Optional<PaymentSystem> paymentSystem;
     List<String> errors;
     
-    CardValidationInfo(boolean isValid, List<String> errors, Optional<PaymentSystem> paymentSystem) {
+    CardValidationInfo(boolean isValid, List<String> errors) {
     	this.isValid = isValid;
     	this.errors = errors;
-    	this.paymentSystem = paymentSystem;
     }
     
     public boolean getIsValid() {
     	return isValid;
-    }
-    
-    public Optional<PaymentSystem> getPaymentSystem() {
-        return paymentSystem;
     }
     
     public List<String> getErrors() {
