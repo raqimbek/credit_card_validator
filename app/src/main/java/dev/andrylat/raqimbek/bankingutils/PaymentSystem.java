31c7a15872565854ce237ce8babb3b1c005c129e
raqimbek <raqimbek@proton.me>
2025-03-16 19:08:14 +0500

    deleted unnecessary constructors and the getPrefixesAsList() getter in the PaymentSystem enum

--- app/src/main/java/dev/andrylat/raqimbek/bankingutils/PaymentSystem.java
+++ app/src/main/java/dev/andrylat/raqimbek/bankingutils/PaymentSystem.java
@@ -1,13 +1,12 @@
 package dev.andrylat.raqimbek.bankingutils;
 
-import java.util.stream.IntStream;
 import java.util.List;
 import java.util.ArrayList;
 import org.apache.commons.lang3.StringUtils;
 
 public enum PaymentSystem {
     VISA("16", List.of(4)),
-    MASTERCARD("16", "51-55"),
+    MASTERCARD("16", List.of(51, 52, 53, 54, 55)),
     DISCOVER("16", List.of(6011, 65)),
     DINERS_CLUB("16", List.of(36, 38)),
     JCB("16", List.of(35)),
@@ -18,11 +17,6 @@ public enum PaymentSystem {
     private List<Integer> prefixList = new ArrayList<>();
 
     private PaymentSystem(String cardLengthRange, List<Integer> prefixes) {
-        this(cardLengthRange);
-        prefixList.addAll(prefixes);
-    }
-
-    private PaymentSystem(String cardLengthRange) {
         if (cardLengthRange.indexOf("-") == -1) {
             cardMaxLength = Integer.valueOf(cardLengthRange).intValue();
         } else if (StringUtils.countMatches(cardLengthRange, "-") == 1) {
@@ -30,18 +24,7 @@ public enum PaymentSystem {
             cardMaxLength = Integer.valueOf(cardLengthRange.substring(cardLengthRange.indexOf("-") + 1)).intValue();
         }
 
-        // idk yet whether it should throw an exception if an argument contains more
-        // than one hyphen
-    }
-
-    private PaymentSystem(String cardLengthRange, String prefixRange) {
-        this(cardLengthRange);
-        if (prefixRange.indexOf("-") != -1) {
-            var rangeEnds = prefixRange.split("-");
-            IntStream.range(Integer.valueOf(rangeEnds[0]), Integer.valueOf(rangeEnds[1]) + 1).forEach(prefix -> {
-                prefixList.add(prefix);
-            });
-        }
+        prefixList.addAll(prefixes);
     }
 
     public int getCardMaxLength() {
@@ -54,10 +37,5 @@ public enum PaymentSystem {
 
     public List<Integer> getPrefixes() {
         return prefixList;
-    }
-
-    public List<List<Integer>> getPrefixesAsLists() {
-        return prefixList.stream().map(n -> List.of(n.toString().split("")).stream().map(Integer::valueOf).toList())
-                .toList();
     }
 }
