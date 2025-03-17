607599c7d96b23dee58ae9e74168b46e845f94ff
raqimbek <raqimbek@proton.me>
2025-03-17 19:59:11 +0500

    refactored message output logic

--- app/src/main/java/dev/andrylat/raqimbek/bankingutils/BankingUtilsApp.java
+++ app/src/main/java/dev/andrylat/raqimbek/bankingutils/BankingUtilsApp.java
@@ -1,6 +1,7 @@
 package dev.andrylat.raqimbek.bankingutils;
 
 import java.util.Scanner;
+import java.util.List;
 
 public class BankingUtilsApp {
     private static CardValidationUserInteraction userInteraction = new CardValidationUserInteraction();
@@ -14,22 +15,26 @@ public class BankingUtilsApp {
     private static void run() {
         var scanner = new Scanner(System.in);
 
-    	userInteraction.write("Hello. Enter card number for validation:", System.out);
+    	userInteraction.write(false, List.of("Hello. Enter card number for validation:"), System.out);
 
     	var userInput = userInteraction.read(scanner);        
     	var cardValidationInfo = cardValidator.checkCardNumber(userInput);
 
     	if (!cardValidationInfo.getIsValid()) {
-    	    var errors = cardValidationInfo.getErrors().toString();
+    	    var errors = cardValidationInfo.getErrors();
 
-    	    userInteraction.write(errors, System.out);
+    	    userInteraction.write(true, errors, System.out);
     	} else {
     	    var paymentSystem = paymentSystemDeterminer
     	            .determinePaymentSystemByCardNumber(userInteraction.getInput())
     	            .get();
 
     	    if (paymentSystem != null) {
-    	        userInteraction.write(paymentSystem.toString(), System.out);
+    	        userInteraction.write(false, List.of(
+    	                new StringBuilder("Card is valid. Payment System is \"")
+    	                    .append(paymentSystem.toString())
+    	                    .append("\"")
+    	                    .toString()), System.out);
     	    }
     	}
     }
