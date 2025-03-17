b56cd03e839846b4ff85be2a59c8191a79eede9f
raqimbek <raqimbek@proton.me>
2025-03-17 19:50:35 +0500

    refactored the CardValidationUserInteraction.write() method

--- app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidationUserInteraction.java
+++ app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidationUserInteraction.java
@@ -7,9 +7,19 @@ import java.io.PrintStream;
 public class CardValidationUserInteraction {
     private String input;
 
-    public void write(List<String> messages, PrintStream out) {
-        messages.stream()
-            .forEachOrdered(out::println);
+    public void write(boolean isErrorMessage, List<String> messages, PrintStream out) {
+        if (isErrorMessage) {
+            out.println("Errors:");
+            messages.stream()
+                .forEachOrdered(m ->
+                    out.println(new StringBuilder("->")
+                            .append(m)
+                            .toString()
+                            ));
+        } else {
+            messages.stream()
+                .forEach(out::println);
+        }
     }
 
     public String read(Scanner scanner) {
