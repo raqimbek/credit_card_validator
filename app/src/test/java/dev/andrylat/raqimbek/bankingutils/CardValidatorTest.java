94b1adf5d1ba0a81ce8c14927a7016715fbed819
raqimbek <raqimbek@proton.me>
2025-03-18 19:36:42 +0500

    added unit tests

--- app/src/test/java/dev/andrylat/raqimbek/bankingutils/CardValidatorTest.java
+++ app/src/test/java/dev/andrylat/raqimbek/bankingutils/CardValidatorTest.java
@@ -1,4 +1,65 @@
 package dev.andrylat.raqimbek.bankingutils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class CardValidatorTest {
+  CardValidator cardValidator = new CardValidator();
+
+  @Test
+  public void shouldReturnInvalidLengthErrorMessage() {
+    var expected = "Length should be 16 symbols";
+    var actual = "";
+    var errors = cardValidator.checkCardNumber("1234").getErrors();
+    if (!errors.isEmpty()) {
+      actual = errors.getFirst();
+    }
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldReturnOnlyDigitsErrorMessage() {
+    var expected = "Card Number must contain only digits";
+    var actual = "";
+    var errors = cardValidator.checkCardNumber("123ab").getErrors();
+    if (!errors.isEmpty()) {
+      actual = errors.getFirst();
+    }
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldReturnNoPaymentSystemErrorMessage() {
+    var expected = "Payment System can't be determined";
+    var actual = "";
+    var errors = cardValidator.checkCardNumber("5625 2334 3010 9903").getErrors();
+    if (!errors.isEmpty()) {
+      actual = errors.getFirst();
+    }
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldReturnLuhnTestErrorMessage() {
+    var expected = "Card Number does not pass the Luhn Test";
+    var actual = "";
+    var errors = cardValidator.checkCardNumber("5425 2334 3010 9923").getErrors();
+    if (!errors.isEmpty()) {
+      actual = errors.getFirst();
+    }
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldReturnNoErrorMessage() {
+    var expected = 0;
+    var errors = cardValidator.checkCardNumber("5425 2334 3010 9903").getErrors();
+    var actual = errors.size();
+
+    assertEquals(expected, actual);
+  }
 }
--- app/src/test/java/dev/andrylat/raqimbek/bankingutils/PaymentSystemDeterminerTest.java
+++ app/src/test/java/dev/andrylat/raqimbek/bankingutils/PaymentSystemDeterminerTest.java
@@ -1,4 +1,45 @@
 package dev.andrylat.raqimbek.bankingutils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class PaymentSystemDeterminerTest {
+  PaymentSystemDeterminer paymentSystemDeterminer = new PaymentSystemDeterminer();
+
+  @Test
+  public void shouldReturnVisa() {
+    var expected = "VISA";
+    var cardNumber = "4263 9826 4026 9299";
+    var actual = getPaymentSystem(cardNumber);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldReturnMasterCard() {
+    var expected = "MASTERCARD";
+    var cardNumber = "5425 2334 3010 9903";
+    var actual = getPaymentSystem(cardNumber);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void shouldReturnEmptyString() {
+      var expected = "";
+      var cardNumber = "5863 9826 4026 9299";
+      var actual = getPaymentSystem(cardNumber);
+
+      assertEquals(expected, actual);
+  }
+
+  private String getPaymentSystem(String cardNumber) {
+    var paymentSystem = paymentSystemDeterminer.determinePaymentSystemByCardNumber(cardNumber);
+
+    if (paymentSystem.isPresent()) {
+      return paymentSystem.get().toString();
+    }
+
+    return "";
+  }
 }
