39a5f6775403473b617470f0c91b8d80170257bd
raqimbek <raqimbek@proton.me>
2025-03-25 16:21:57 +0500

    Created test for MortgageCalculator

---
+++ app/src/test/java/dev/andrylat/raqimbek/bankingutils/MortgageCalculatorTest.java
@@ -0,0 +1,13 @@
+package dev.andrylat.raqimbek.bankingutils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MortgageCalculatorTest {
+  @Test
+  public void shouldReturnProperPayment() {
+    var expected = 599.55;
+    var actual = 0.0;
+    assertEquals(expected, actual);
+  }
+}
