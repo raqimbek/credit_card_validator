5a83d219114323d438f12463ccb4e930d8a05dc5
raqimbek <raqimbek@proton.me>
2025-03-27 17:11:16 +0500

    changed the type of CardValidationInfo to a record type

--- app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidationInfo.java
+++ app/src/main/java/dev/andrylat/raqimbek/bankingutils/CardValidationInfo.java
@@ -2,20 +2,4 @@ package dev.andrylat.raqimbek.bankingutils;
 
 import java.util.List;
 
-public class CardValidationInfo {
-  private final boolean isValid;
-  private final List<String> errors;
-
-  public CardValidationInfo(boolean isValid, List<String> errors) {
-    this.isValid = isValid;
-    this.errors = errors;
-  }
-
-  public boolean getIsValid() {
-    return isValid;
-  }
-
-  public List<String> getErrors() {
-    return errors;
-  }
-}
+public record CardValidationInfo(boolean isValid, List<String> errors) {}
