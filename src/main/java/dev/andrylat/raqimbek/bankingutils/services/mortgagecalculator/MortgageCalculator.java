fc9fa8ded6e7ce87b7c9a5e146e8861347b70994
raqimbek <raqimbek@proton.me>
2025-04-04 08:09:11 +0500

    changed the return type to BigDecimal

--- src/main/java/dev/andrylat/raqimbek/bankingutils/services/mortgagecalculator/MortgageCalculator.java
+++ src/main/java/dev/andrylat/raqimbek/bankingutils/services/mortgagecalculator/MortgageCalculator.java
@@ -1,20 +1,30 @@
 package dev.andrylat.raqimbek.bankingutils.services.mortgagecalculator;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 public class MortgageCalculator {
-  public Long calculateMonthlyMortgagePayment(List<String> inputList) {
-    var borrowedAmount = Double.parseDouble(inputList.get(0));
-    var annualInterestRate = Double.parseDouble(inputList.get(1));
-    var numberOfYears = Double.parseDouble(inputList.get(2));
-    annualInterestRate /= 100;
+  public BigDecimal calculateMonthlyMortgagePayment(List<String> inputList) {
+    var borrowedAmount = new BigDecimal(inputList.get(0));
+    var annualInterestRate = new BigDecimal(inputList.get(1));
+    var numberOfYears = new BigDecimal(inputList.get(2));
+    annualInterestRate = annualInterestRate.divide(new BigDecimal(100), RoundingMode.HALF_EVEN);
 
     var payBackForEveryCurrencyUnit =
-        1
-            + annualInterestRate * numberOfYears / 2
-            + Math.pow(annualInterestRate * numberOfYears, 2) / 12;
-    var numberOfMonthlyPayments = numberOfYears * 12;
-
-    return Math.round(borrowedAmount * payBackForEveryCurrencyUnit / numberOfMonthlyPayments);
+        BigDecimal.ONE
+            .add(
+                annualInterestRate
+                    .multiply(numberOfYears)
+                    .divide(new BigDecimal(2), RoundingMode.HALF_EVEN))
+            .add(
+                annualInterestRate
+                    .multiply(numberOfYears)
+                    .pow(2)
+                    .divide(new BigDecimal(12), RoundingMode.HALF_EVEN));
+    var numberOfMonthlyPayments = numberOfYears.multiply(new BigDecimal(12));
+    return borrowedAmount
+        .multiply(payBackForEveryCurrencyUnit)
+        .divide(numberOfMonthlyPayments, RoundingMode.HALF_EVEN);
   }
 }
